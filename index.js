2d4c87648b45acc26d04a945137b6729014a29b7
ali boulajine <abdelaliboulajine@gmail.com>
2017-05-11 14:56:57 +0200

    Revert "🐼 a new service"

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ app.get('/add/:a/:b', (req, res) => {
   });
 });
 
-app.get('/multiply/:a/:b', (req, res) => {
-    res.send({
-    result: calc.multiply(Number(req.params.a), Number(req.params.b))
-});
-});
 
 
 
